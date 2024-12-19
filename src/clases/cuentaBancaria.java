f592b7340c6fb4118375e1d88a30cdd5a16b4e08
jorge <ogomez.isc@gmail.com>
2024-12-19 07:41:33 -0600

    Primer commit

---
+++ src/clases/cuentaBancaria.java
@@ -0,0 +1,11 @@
+package src.clases;
+
+import java.util.ArrayList;
+
+public class cuentaBancaria {
+    private String numeroCuenta;
+    private double saldo;
+    private ArrayList<Transaccion> transacciones;
+
+
+}

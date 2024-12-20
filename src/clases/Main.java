f1bf5ba2db7d72dfdaeba516cf0443c0677c198f
jorge <ogomez.isc@gmail.com>
2024-12-20 13:35:07 -0600

    Esta inhabilitado el inicio de sesion

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -10,14 +10,16 @@ public class Main {
         cuentas.add(new Cuenta("1234567890123456", "1234", 10000.0, "debito"));
         cuentas.add(new Cuenta("6543210987654321", "5678", 20000.0, "credito"));
 
+        Cuenta cuenta = cuentas.get(0);
+
         // Crear tarjeta de crédito
         TarjetaCredito credito = new TarjetaCredito("1478523691234567", "04/12", 1200000.0);
 
         // Crear el cajero con la lista de cuentas
-        Cajero cajero = new Cajero(cuentas, credito);
+        Cajero cajero = new Cajero(cuenta, credito);
 
         // Iniciar sesión y mostrar el menú
-        cajero.iniciarSesion();
+        //cajero.iniciarSesion();
         cajero.mostrarMenu();
     }
 }

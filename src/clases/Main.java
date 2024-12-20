b8192b22963112552597faa907dc9ac290201d76
kareltonatiuh <137120152+kareltonatiuh@users.noreply.github.com>
2024-12-20 00:32:08 -0600

    Update Main.java

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -6,6 +6,7 @@ import src.clases.Cuenta;
 public class Main {
     public static void main(String[] args) {
         Cuenta cuenta = new Cuenta("1234567890123456", "1234", 10000.0, "debito");
+        TarjetaCredito credito = new TarjetaCredito("1478523691234567", "04/12", 1200000.0);
         Cajero cajero = new Cajero(cuenta);
         cajero.mostrarMenu();
     }

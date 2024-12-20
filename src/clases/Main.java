448d6c8a03aa9d2f7d660f36e21d4f006e1a824d
Diego Alberto Hernández Reyes <167047846+diego-hernandez-27@users.noreply.github.com>
2024-12-20 10:18:29 -0600

    Nueva cuenta
    Puse un ArrayList para introducir nuevas cuentas

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -1,17 +1,23 @@
 package src.clases;
 
-import src.clases.Cajero;
-import src.clases.Cuenta;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Creación de las cuentas
+        List<Cuenta> cuentas = new ArrayList<>();
+        cuentas.add(new Cuenta("1234567890123456", "1234", 10000.0, "debito"));
+        cuentas.add(new Cuenta("6543210987654321", "5678", 20000.0, "credito"));
 
-        Cuenta cuenta = new Cuenta("1234567890123456", "1234", 10000.0, "debito");
+        // Crear tarjeta de crédito
         TarjetaCredito credito = new TarjetaCredito("1478523691234567", "04/12", 1200000.0);
-        Cajero cajero = new Cajero(cuenta, credito);
 
-        cajero.ValidarCuenta();
+        // Crear el cajero con la lista de cuentas
+        Cajero cajero = new Cajero(cuentas, credito);
+
+        // Iniciar sesión y mostrar el menú
+        cajero.iniciarSesion();
         cajero.mostrarMenu();
     }
 }

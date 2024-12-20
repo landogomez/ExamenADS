e76185bc517889e61f32a583a427d098b67ff46d
Zentrx <sebastian.absalon2120@gmail.com>
2024-12-20 03:28:39 -0600

    Validación de Cuenta y Nip

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -2,12 +2,16 @@ package src.clases;
 
 import src.clases.Cajero;
 import src.clases.Cuenta;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Cuenta cuenta = new Cuenta("1234567890123456", "1234", 10000.0, "debito");
         TarjetaCredito credito = new TarjetaCredito("1478523691234567", "04/12", 1200000.0);
-        Cajero cajero = new Cajero(cuenta);
+        Cajero cajero = new Cajero(cuenta, credito);
+
+        cajero.ValidarCuenta();
         cajero.mostrarMenu();
     }
 }
